e5f70abb44309f8f829b12973be2a6490335c648
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2024-07-05 00:11:52 +0200

    struttura chiamante da main dei file fatta!

--- main.c
+++ main.c
@@ -17,8 +17,30 @@ int main(){
         char* cmd = strtok(command, " ");
         if(cmd == NULL) continue;
 
-        // function handlers here
+        if(strcmp(cmd, "touch") == 0){
+            const char* file_name = strtok(NULL, " ");
+            if(file_name) handle_create_file(fs, file_name);
+            else printf("Usage: touch <file_name>\n");
+        }
 
+        if(strcmp(cmd, "rm") == 0){
+            const char* file_name = strtok(NULL, " ");
+            if(file_name) handle_erase_file(fs, file_name);
+            else printf("Usage: rm <file_name>\n");
+        }
+
+        if(strcmp(cmd, "wof") == 0){
+            const char* file_name = strtok(NULL, " ");
+            const char* data = strtok(NULL, "");
+            if(file_name && data) handle_write_file(fs, file_name, data);
+            else printf("Usage: wof <file_name> <data>\n");
+        }
+
+        if(strcmp(cmd, "cat") == 0){
+            const char* file_name = strtok(NULL, " ");
+            if(file_name) handle_read_file(fs, file_name);
+            else printf("Usage: cat <file_name>\n");
+        }
     }
 
     
